badba8475b1c205bc0aa15264dd120f0f9b1fb6a
Nate Ferrero <nate@tagme.in>
2024-09-02 17:12:36 -0400

    auto size text vertically

--- public/src/components/text.js
+++ public/src/components/text.js
@@ -1,5 +1,5 @@
 globalThis.LOAD['components/text'].resolve(async function ({ load }) {
- return (text, options) => { 
+ return (text, options) => {
   const element = document.createElement('span')
   element.textContent = text
   element.classList.add('--components-text--container')
@@ -12,10 +12,12 @@ globalThis.LOAD['components/text'].resolve(async function ({ load }) {
    }
    function measure() {
     const fontSize = computedStyle(element, 'font-size')
-    const unitPlace = 1 + [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((a, n) => {
-     const index = fontSize.lastIndexOf(n.toString(10))
-     return Math.max(a, index)
-    }, 0)
+    const unitPlace =
+     1 +
+     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((a, n) => {
+      const index = fontSize.lastIndexOf(n.toString(10))
+      return Math.max(a, index)
+     }, 0)
     const currentFontSizeUnit = fontSize.slice(unitPlace)
     if (currentFontSizeUnit !== 'px') {
      console.warn('autosize: font size must be in px')
@@ -24,13 +26,30 @@ globalThis.LOAD['components/text'].resolve(async function ({ load }) {
     const currentFontSize = parseFloat(fontSize.slice(0, unitPlace))
     const currentSize = element.getBoundingClientRect()
     const currentViewportSize = viewport.element.getBoundingClientRect()
-    const currentPaddingLeft = parseFloat(computedStyle(viewport.element, 'padding-left'))
-    const currentPaddingRight = parseFloat(computedStyle(viewport.element, 'padding-right'))
-    const fillX = (currentViewportSize.width - currentPaddingLeft - currentPaddingRight) / currentSize.width
-    element.style.fontSize = `${fillX * currentFontSize}${currentFontSizeUnit}`
-    element.style.transform = `scale(${1 / fillX})`
-    setTimeout(function () {    
-     element.style.transition = `transform 0.75s ease-out`
+    const currentPaddingLeft = parseFloat(
+     computedStyle(viewport.element, 'padding-left')
+    )
+    const currentPaddingRight = parseFloat(
+     computedStyle(viewport.element, 'padding-right')
+    )
+    const fillX =
+     (currentViewportSize.width - currentPaddingLeft - currentPaddingRight) /
+     currentSize.width
+    const currentPaddingTop = parseFloat(
+     computedStyle(viewport.element, 'padding-top')
+    )
+    const currentPaddingBottom = parseFloat(
+     computedStyle(viewport.element, 'padding-bottom')
+    )
+    const fillY =
+     (currentViewportSize.height - currentPaddingTop - currentPaddingBottom) /
+     currentSize.height
+    const scale = Math.min(fillX, fillY)
+    console.log({ fillY, fillX, scale })
+    element.style.fontSize = `${scale * currentFontSize}${currentFontSizeUnit}`
+    element.style.transform = `scale(${1 / scale})`
+    setTimeout(function () {
+     element.style.transition = `transform 0.25s ease-out`
      element.style.transform = `scale(1)`
     }, 120)
    }
@@ -39,4 +58,3 @@ globalThis.LOAD['components/text'].resolve(async function ({ load }) {
   return { element, autoSize }
  }
 })
-
